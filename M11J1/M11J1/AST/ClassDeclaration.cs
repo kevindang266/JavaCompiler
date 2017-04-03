48fe3aa74603d34b382a314f766ac3478b40b75f
Vinay2504 <vinay.ramachandra@connect.qut.edu.au>
2017-04-03 10:45:30 +1000

    Added Class declaration

--- M11J1/M11J1/AST/ClassDeclaration.cs
+++ M11J1/M11J1/AST/ClassDeclaration.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace M11J1.AST
 {
-    public class ClassDeclaration
+   public class ClassDeclaration : Node
     {
+        
+        /* NormalClassDeclaration:
+        {ClassModifier} class Identifier [TypeParameters] [Superclass] [Superinterfaces] ClassBody */
+
+        private ClassModifier _classModifier;
+        private string _className;
+    
+        public ClassDeclaration( ClassModifier classModifier, string className)
+        {
+            this._classModifier = classModifier;
+            this._className = className;
+            
+        }
     }
 }
