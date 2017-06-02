661b3a6f38701ced3dbab339be5822981fd141fd
azadthakur <azad.kumar@connect.qut.edu.au>
2017-06-02 15:25:23 +1000

    Create LexicalScope.cs

---
+++ Personal Folders/Azad/LexicalScope.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M11J1.AST
+{
+    public class LexicalScope
+    {
+        private LexicalScope _parentScope;
+        public Dictionary<string, IDeclaration> SymbolTable { get; set; }
+
+        public LexicalScope()
+        {
+            _parentScope = null;
+            SymbolTable = new Dictionary<string, IDeclaration>();
+        }
+
+        public void SetParentScope(LexicalScope scope)
+        {
+            _parentScope = scope;
+        }
+
+        public IDeclaration ResolveHere(string symbol)
+        {
+            return SymbolTable.FirstOrDefault(x => x.Key == symbol).Value;
+        }
+
+        public IDeclaration Resolve(string symbol)
+        {
+            IDeclaration local = ResolveHere(symbol);
+            if (local != null)
+                return local;   // return VariableDeclaration object if it can be found in current scope
+            return _parentScope?.Resolve(symbol);
+        }
+    }
+}
