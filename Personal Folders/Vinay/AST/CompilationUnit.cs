2cf0045e54085058647b5fb4ca6e72aad9f295f0
Vinay2504 <vinay.ramachandra@connect.qut.edu.au>
2017-06-02 18:24:42 +1000

    Updated CompilationUnit

--- Personal Folders/Vinay/AST/CompilationUnit.cs
+++ Personal Folders/Vinay/AST/CompilationUnit.cs
@@ -3,10 +3,13 @@
     public class CompilationUnit : Node
     {
         private ClassDeclaration _classDeclaration;
+        private string _compilationName;
 
-        public CompilationUnit (ClassDeclaration classDeclaration)
+        public CompilationUnit(ClassDeclaration classDeclaration)
         {
+            _compilationName = "Test";
             _classDeclaration = classDeclaration;
+            _classDeclaration.CompilationName = _compilationName;
         }
 
         public override void Dump(int indent)
@@ -23,6 +26,12 @@
         public override void TypeCheck()
         {
             _classDeclaration.TypeCheck();
+        }
+
+        public override void GenCode(string file)
+        {
+            Emit(file, ".assembly {0} {{}}", _compilationName);
+            _classDeclaration.GenCode(file);
         }
     }
 }
